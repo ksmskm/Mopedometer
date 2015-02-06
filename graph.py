d52d89f327d75dc08d09d6056072ef0d5f1fa0ea
ksmskm <ksmarvich@gmail.com>
2015-02-06 11:41:37 -0800

    switching branches

--- graph.py
+++ graph.py
@@ -1,31 +1,16 @@
-import sqlite3
-import time
-import datetime
+#!/usr/bin/env python
+# Let printing work the same in Python 2 and 3
+from __future__ import print_function
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+import sqlite3
 
 conn = sqlite3.connect('test.db')
+
 curs = conn.cursor()
-graphArray = []
 
+print("\nEntire database contents:\n")
 for row in curs.execute("SELECT * FROM temps"):
-#    startingInfo = str(row).replace(')','').replace('(','').replace('u\'','').replace("'","")
- #   splitInfo = startingInfo.split(',')
-  #  for col in range(len(splitInfo)):
-#	print splitInfo[col] + '_'
-    print 'row'
+    print(row)
 
-#    graphArrayAppend = splitInfo[0] + ',' + splitInfo[1]
-#    graphArray.append(graphArrayAppend)
+conn.close()
 
-#timestamp, value = np.loadtxt(graphArray,delimiter = ',', unpack = True, converters = { 0: mdates.strpdate2num(' %H:%M:%S')})
-
-#fig = plt.figure()
-
-#rect = fig.patch
-
-#ax1 = fig.add_subplot(1,1,1, axisbg='white')
-#plt.plot_date(x = timestamp, y = value, fmt='b-', label = 'value', linewidth=2)
-#plt.show()
--- read.py
+++ read.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+# Let printing work the same in Python 2 and 3
+from __future__ import print_function
 
 import sqlite3
 
-conn=sqlite3.connect('test.db')
+conn = sqlite3.connect('test.db')
 
-curs=conn.cursor()
+curs = conn.cursor()
 
-print "\nEntire database contents:\n"
+print("\nEntire database contents:\n")
 for row in curs.execute("SELECT * FROM temps"):
-    print row
+    print(row)
 
 conn.close()
