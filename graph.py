af9a26528e2e977cd319f729b37e5320cb953598
ksmskm <ksmarvich@gmail.com>
2015-02-10 11:57:53 -0800

    tracking graph.py

--- graph.py
+++ graph.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
-# Let printing work the same in Python 2 and 3
-from __future__ import print_function
+
+import cgitb
+cgitb.enable()
 
 import sqlite3
+import os
+import tempfile
+# write matplotlib files to writable directory
+os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
+
+import matplotlib as mpl
+# 'Agg' backend doesn't require a Display
+mpl.use('Agg')
 
 conn = sqlite3.connect('test.db')
-
 curs = conn.cursor()
 
-print("\nEntire database contents:\n")
+print "\nEntire database contents:\n"
 for row in curs.execute("SELECT * FROM temps"):
-    print(row)
+    print row
+    startingInfo = str(row).replace(')','').replace('(','').replace('u\'','').replace("'","")
+    splitInfo = startingInfo.split(',')
 
 conn.close()
-
