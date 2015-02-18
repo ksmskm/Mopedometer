feb0141d09d11b5cc52c5efe961c9c4d8f659163
ksmskm <ksmarvich@gmail.com>
2015-02-18 15:19:01 -0800

    fixed indentation

--- cylinder.py
+++ cylinder.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import time
 import math
 import os
@@ -9,38 +10,38 @@ DEBUG = 1
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True) 
+        GPIO.output(clockpin, False) 
+        adcout <<= 1 
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
 
-        GPIO.output(cspin, True)
+    GPIO.output(cspin, True)
         
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+    adcout >>= 1 # first bit is 'null' so drop it 
+    return adcout
 
 # Convert celsius to fahrenheit.
 def c_to_f(c):
@@ -70,7 +71,7 @@ def temp_get(value):
 
     t2 = math.pow(c2,3) # c[ln(ohm)]^3
 
-    temp = 1/(a + t1 + t2) #calcualte temperature
+    temp = 1/(a + t1 + t2) #calculate temperature
 
     tempc = temp - 273.15 - 4 #K to C
     # the -4 is error correction for bad python math
@@ -96,13 +97,10 @@ GPIO.setup(SPICS, GPIO.OUT)
 potentiometer_adc = 0;
 
 while True:
-    # we'll assume that the pot didn't move
-    trim_pot_changed = False
-
     # read the analog pin
     trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
 	print trim_pot
 	print c_to_f(temp_get(trim_pot))
 
-        # hang out and do nothing for a half second
-        time.sleep(0.5)
+    # hang out and do nothing for a half second
+    time.sleep(0.5)
